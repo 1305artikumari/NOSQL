5aafedee477a2fb2f7cdfa14bbaa054eedded3db
Arti Kumari <artikumari09011999@gmail.com>
2023-12-28 22:40:15 +0530

    Creating the Users table

---
+++ Assignment4/app.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5baa2528563f16379fc8a610')
+    .then(user => {
+      
+      if (user !== null && user !== undefined) {
+        // Access user properties safely
+        req.user = new User(user.name, user.email, user.cart, user._id);
+      } else {
+        console.log('User not found or is null.');
+      }
+      
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
